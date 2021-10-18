6a68b4229953988b8e6344833f94f9dd3943c3fd
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2021-10-18 15:31:18 +0300

    14:20 npm i @nestjs/config ConfigModule.forRoot({ envFilePath: '.env' }), .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from "@nestjs/common";
 import { SequelizeModule } from '@nestjs/sequelize';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
   controllers: [],
   providers: [],
   imports: [
+    ConfigModule.forRoot({
+
+      envFilePath: '.env'
+    }),
     SequelizeModule.forRoot({
       dialect: 'postgres',
       host: 'localhost',
