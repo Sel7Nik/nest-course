a33b5f6e20e5333605ed8e933b90227324678183
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2021-10-18 17:24:49 +0300

    21:10  @Column @Column({ type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true }) @Column({type: DataType.STRING, unique: true, allowNull: false}) @Column({type: DataType.STRING, allowNull: false}) @Column({type: DataType.BOOLEAN, defaultValue: false}) @Column({type: DataType.STRING, allowNull: true})

---
+++ src/users/users.model.ts
@@ -0,0 +1,25 @@
+
+import { BelongsToMany, Column, DataType, HasMany, Model, Table } from "sequelize-typescript";
+
+@Table({ tableName: 'users' })
+export class User extends Model<User,> {
+
+  @Column({ type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true })
+  id: number;
+
+  @Column({ type: DataType.STRING, unique: true, allowNull: false })
+  email: string;
+
+  @Column({ type: DataType.STRING, allowNull: false })
+  password: string;
+
+  @Column({ type: DataType.BOOLEAN, defaultValue: false })
+  banned: boolean;
+
+  @Column({ type: DataType.STRING, allowNull: true })
+  banReason: string;
+
+  roles: Role[];
+
+  posts: Post[];
+}
