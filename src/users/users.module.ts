40e0b45b6b9ab77b3fe99a41a5ad116317a048a5
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2021-10-22 23:24:27 +0300

    1:01:00 JwtAuthGuard

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,18 +1,20 @@
 import { SequelizeModule } from '@nestjs/sequelize';
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { User } from './users.model';
 import { UserRoles } from 'src/roles/user-roles.model';
 import { Role } from 'src/roles/roles.model';
 import { RolesModule } from 'src/roles/roles.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
   imports: [
     SequelizeModule.forFeature([User, Role, UserRoles]),
-    RolesModule
+    RolesModule,
+    forwardRef(() => AuthModule)
   ],
   exports: [
     UsersService
